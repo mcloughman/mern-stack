9a1ad7605eb7acfb0d1158ba07bd48d8c2ed8e98
Michael Loughman <mcloughman@gmail.com>
2022-08-09 19:09:36 -0700

    Create controllers folder and workoutControllers file. We create functions that contain the appropriate code for our first three route handlers. We will do the others next. Then in our routes/workouts.js we just call the function in each route handler. Much neater.

---
+++ controllers/workoutControllers.js
@@ -0,0 +1,51 @@
+const Workout = require("../models/workoutModel");
+const mongoose = require("mongoose");
+
+// get all workouts
+const getWorkouts = async (req, res) => {
+  try {
+    const workouts = await Workout.find({}).sort({ createdAt: -1 });
+    res.status(200).json({ workouts });
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+};
+
+// get a single workout
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
+  // the below if statement will at least prevent network error if id is invalid mongoose type
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ err: "No such workout" });
+  }
+  const workout = await Workout.findById(id);
+
+  if (!workout) {
+    return res.status(404).json({ err: "No such workout" });
+  }
+  res.status(200).json(workout);
+};
+
+// create new workout
+const createWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  // add doc to db
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (err) {
+    res.status(400).json({ err: err.message });
+  }
+};
+
+// delete a workout
+
+// update a workout
+
+module.exports = {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+};
