432e97cf4974ecabef9f226eda00ef4f2116a867
Michael Loughman <mcloughman@gmail.com>
2022-08-09 18:21:37 -0700

    Set up connection with mongodb atlas. Create a models folder with a workoutModel file. Install mongoose and workout schema. Create a new Workout and test it via postman. This commit is for sections 4 and 5 of NetNinja MERN stack youtube course.

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const workoutRoutes = require("./routes/workouts");
+
+// express app
 const app = express();
 
 // middleware
-app.use(express.json());
+app.use(express.json()); // any request that comes in with data will be parsed and attached to request obj
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
+// routes
 app.use("/api/workouts", workoutRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("Listening on port ", process.env.PORT);
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to db! Listening on port", process.env.PORT);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
